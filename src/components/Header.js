332fa96648fcdb62a764616b57db76707e410c1c
lsancho <lsanchoaragon@outlook.com>
2021-05-23 23:59:08 +0200

    Añadido responsive de personajes screen y algunos estilos

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
 		<div className="pS__container animate__animated animate__fadeIn">
-			<h1>GenshinBuilds</h1>
+			<h1 className="head__title">GenshinBuilds</h1>
         </div>
     )
 }
